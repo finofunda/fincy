8d2e4499c532d9bbfba692cbd9b5b36c5b742bf4
E33605 <debmalyapramanik.005@gmail.com>
2026-01-07 12:02:29 +0530

    ✨ create base abstract class for financial metrics - provide base attributes name, currency etc. - provide abstract methods .calculate() to standardize

---
+++ fincy/core/__init__.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+
+"""
+The module follows object-oriented structure and all the methods are
+an instance of one of the base abstract class that follows model and
+field validators using :mod:`pydantic` module. Check the ``_base.py``
+for more information on the base abstract class.
+"""
+
